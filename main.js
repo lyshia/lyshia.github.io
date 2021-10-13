9ea7c3a4d436c1ed363366e3287357584b4b33c7
alyshia bentley <alyshiabentley@gmail.com>
2021-10-13 15:02:45 -0700

    add experienced programs list

--- main.js
+++ main.js
@@ -25,3 +25,21 @@ $('#alyshiaPortrait').mouseover(function () {
 $('#alyshiaPortrait').mouseout(function () {
 	$(this).attr('src', 'img/portrait.jpeg');
 });
+
+//add icons to experience list
+const brands  = [ 'img/brands/angularjs.png' ,
+'img/brands/bitbucket-48x48-2284628.png',
+'img/brands/css-48x48-2284638.png',
+'img/brands/git-48x48-2284657.png',
+'img/brands/jira-48x48-2284680.png',
+'img/brands/jquery-48x48-2284682.png',
+'img/brands/nodejs-48x48-2284703.png',
+'img/brands/npm-48x48-2284704.png',
+'img/brands/python-48x48-2284717.png',
+'img/brands/react-48x48-2284721.png',
+'img/brands/visual-studio-48x48-2284769.png'
+];
+//loop to show each brand i have experience iwth
+$.each( brands, function(index, brand) {
+ $('#experienceList').append('<li><img src=' + brand + '></li>');
+})
