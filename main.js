a7758de13507005495704aef9ab5a34c4cffb627
alyshia bentley <alyshiabentley@gmail.com>
2021-10-14 14:40:24 -0700

    add Hello in different languages

--- main.js
+++ main.js
@@ -16,6 +16,19 @@ $('#submitButton').click(function () {
 	// });
 });
 
+// load "hello" file and then print out random hello
+// API didn't do what I liked, so I parsed from https://fourtonfish.com/project/hellosalut-api/
+ setInterval ( function(){
+$.getJSON('hello.json', function (response) {
+	//get length of the array
+	let randomIndex = Math.floor(Math.random() * response.hello.length)
+	// can't read HTML code, so have to parse the selection to HTML
+	let wordstoHTML = $.parseHTML(response.hello[randomIndex])
+	//print out the selected word in HTML
+	$('#helloWorld').text(' ' + wordstoHTML[0].textContent + '');
+})}, 5000);
+
+//set profile picture
 $('#alyshiaPortrait').attr('src', 'img/portrait.jpeg');
 
 // when mouse over my portrait, have a special surprise
@@ -29,41 +42,46 @@ $('#alyshiaPortrait').mouseout(function () {
 });
 
 //add icons to experience list
-const logos  = [
-'img/brands/angularjs.png',
-'img/brands/bitbucket-48x48-2284628.png',
-'img/brands/css-48x48-2284638.png',
-'img/brands/git-48x48-2284657.png',
-'img/brands/jira-48x48-2284680.png',
-'img/brands/jquery-48x48-2284682.png',
-'img/brands/nodejs-48x48-2284703.png',
-'img/brands/python-48x48-2284717.png',
-'img/brands/react-48x48-2284721.png'
+const logos = [
+	'img/brands/angularjs.png',
+	'img/brands/bitbucket-48x48-2284628.png',
+	'img/brands/css-48x48-2284638.png',
+	'img/brands/git-48x48-2284657.png',
+	'img/brands/jira-48x48-2284680.png',
+	'img/brands/jquery-48x48-2284682.png',
+	'img/brands/nodejs-48x48-2284703.png',
+	'img/brands/python-48x48-2284717.png',
+	'img/brands/react-48x48-2284721.png',
 ];
 //loop to show each brand i have experience with
-$.each( logos, function(index, logo) {
- $('#experienceList').append('<li><img src=' + logo + '></li>');
-})
+$.each(logos, function (index, logo) {
+	$('#experienceList').append('<li><img src=' + logo + '></li>');
+});
 
 const $socials = [
 	{
 		name: 'linkedin',
-		image:'img/brands/linkedin-48x48-2284689.png',
-		url:'https://www.linkedin.com/in/alyshiabentley/'
+		image: 'img/brands/linkedin-48x48-2284689.png',
+		url: 'https://www.linkedin.com/in/alyshiabentley/',
 	},
 	{
 		name: 'github',
-	    image:'img/brands/github-48x48-2284658.png', 
-		url:'https://github.com/lyshia',
+		image: 'img/brands/github-48x48-2284658.png',
+		url: 'https://github.com/lyshia',
 	},
 	{
 		name: 'twitter',
 		image: 'img/brands/twitter-48x48-2284759.png',
-		url:'https://twitter.com/BentleyAlyshia'
-	}
+		url: 'https://twitter.com/BentleyAlyshia',
+	},
 ];
 
-
-$socials.forEach( social => {
-	$('#socialsList').append('<li><a href=' + social.url +'><img src=' + social.image + '></a></li>');
-})
+$socials.forEach((social) => {
+	$('#socialsList').append(
+		'<li><a href=' +
+			social.url +
+			' target ="_blank"><img src=' +
+			social.image +
+			'></a></li>'
+	);
+});
